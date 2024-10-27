56f00b53b0f4271b575fd4b1793c6bcf06ce5eb8
Huy <huynhngochuy2803@gmail.com>
2024-10-27 21:29:10 +0700

    react music

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import useMusicPlayer from "../hooks/usePlayerProvider";
+
+function Header() {
+  const { currentTrackName } = useMusicPlayer();
+  return (
+    <div className="header">
+        <marquee behavior="scroll" direction="right">
+            {currentTrackName}
+        </marquee>
+    </div>
+  );
+}
+
+export default Header;
